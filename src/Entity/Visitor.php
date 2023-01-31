df6f597c5ebfd4064a4db7eeb29ac5f15937750d
Petar Stoqnov <desireon98@gmail.com>
2023-01-31 16:19:43 +0200

    Add unique entity validation to session property.

--- src/Entity/Visitor.php
+++ src/Entity/Visitor.php
@@ -7,9 +7,11 @@ use App\Repository\VisitorRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: VisitorRepository::class)]
 #[ApiResource]
+#[UniqueEntity('session')]
 class Visitor
 {
     #[ORM\Id]
