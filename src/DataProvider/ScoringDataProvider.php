b754699165e77d082a2640dc9b9157eb65e3ceea
Petar Stoqnov <desireon98@gmail.com>
2023-02-01 11:10:06 +0200

    Create a data provider that handles the scoring api execution.

---
+++ src/DataProvider/ScoringDataProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataProvider;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\Model\Scoring;
+use App\Repository\VisitorRepository;
+use App\Service\ScoringCalculator;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ScoringDataProvider implements ProviderInterface
+{
+    public function __construct(
+        private readonly VisitorRepository $visitorRepository,
+        private readonly ScoringCalculator $calculator
+    )
+    {
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        $visitor = $this->visitorRepository->findOneBy(['session' => $uriVariables['sessionId']]);
+        if (is_null($visitor)) {
+            throw new NotFoundHttpException();
+        }
+
+        $scoring = new Scoring();
+        $scoring->setSessionId($visitor->getSession());
+        $scoring->setVisitor($visitor);
+
+        $this->calculator->calculate($scoring);
+
+        return $scoring;
+    }
+}
