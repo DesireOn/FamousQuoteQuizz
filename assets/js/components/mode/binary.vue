77e6e40d40c42dc1c34e0c4efcf9c5c3d5727434
Petar Stoqnov <desireon98@gmail.com>
2023-02-04 21:55:33 +0200

    Create a new component for visualizing binary mode.

---
+++ assets/js/components/mode/binary.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="d-flex justify-space-around">
+    <v-btn variant="outlined">
+      Binary (Yes/No) Mode
+    </v-btn>
+    <v-btn variant="plain">
+      Multiple choice Mode
+    </v-btn>
+  </div>
+  <div class="d-flex center mt-3">
+    <v-card class="mx-auto">
+      <v-card-text>"{{ visitor.nextQuestion.name }}"</v-card-text>
+    </v-card>
+  </div>
+
+  <div class="d-flex flex-column align-center justify-center mx-auto mt-3">
+    <v-card-text>
+      {{ selectedSuggestion.answer.name }}?
+    </v-card-text>
+  </div>
+
+  <div class="d-flex justify-space-around align-center flex-column flex-md-row fill-height">
+    <v-btn color="green">
+      Yes
+    </v-btn>
+    <v-btn rounded="lg" color="red">
+      No
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'binary',
+  props: {
+    visitor: {
+      type: Object,
+      required: true
+    },
+  },
+  setup(props) {
+    const suggestions = props.visitor.nextQuestion.questionSuggestions;
+    const selectedSuggestion = suggestions[Math.floor(Math.random() * suggestions.length)];
+
+    return {
+      selectedSuggestion
+    }
+  }
+}
+</script>
