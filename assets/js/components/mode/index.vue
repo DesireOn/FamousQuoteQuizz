01ec207c9faf2df18612090bc41ca8bf2ff88450
Petar Stoqnov <desireon98@gmail.com>
2023-02-04 14:40:38 +0200

    Move viewMode.vue to a new directory.

---
+++ assets/js/components/mode/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="visitor && visitor.settings && visitor.settings.view_mode === 'binary'">
+    <v-btn variant="outlined">
+      Binary (Yes/No)
+    </v-btn>
+    <v-btn variant="plain">
+      Multiple choice
+    </v-btn>
+  </div>
+  <div v-if="visitor && visitor.settings && visitor.settings.view_mode === 'multiple_choice'">
+    <multiple-choice :visitor="visitor"/>
+  </div>
+</template>
+
+<script>
+
+import MultipleChoice from "./multiple-choice.vue";
+
+export default {
+  name: 'viewMode',
+  components: {MultipleChoice},
+  props: {
+    visitor: {
+      type: Object,
+      required: true
+    },
+  },
+};
+</script>
