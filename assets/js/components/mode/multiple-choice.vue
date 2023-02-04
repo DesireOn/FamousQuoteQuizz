f20cc30b4b28e9a8a827d67e145b635f208f11e6
Petar Stoqnov <desireon98@gmail.com>
2023-02-04 14:41:20 +0200

    Create new component for multiple choice mode.

---
+++ assets/js/components/mode/multiple-choice.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="d-flex justify-space-around">
+    <v-btn variant="outlined">
+      Multiple choice
+    </v-btn>
+    <v-btn variant="plain">
+      Binary (Yes/No)
+    </v-btn>
+  </div>
+  <div class="d-flex center mt-3">
+    <v-card class="mx-auto">
+      <v-card-text>"{{ visitor.nextQuestion.name }}"</v-card-text>
+    </v-card>
+  </div>
+  <div class="d-flex flex-column align-center justify-center mx-auto mt-3">
+    <v-btn
+        class="mb-3"
+        v-for="questionSuggestion in visitor.nextQuestion.questionSuggestions"
+        :key="questionSuggestion['@id']"
+        variant="tonal"
+    >
+      {{ questionSuggestion.answer.name }}
+    </v-btn>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'multipleChoice',
+  props: {
+    visitor: {
+      type: Object,
+      required: true
+    },
+  },
+};
+</script>
