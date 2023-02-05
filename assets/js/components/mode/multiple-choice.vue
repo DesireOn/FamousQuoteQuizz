fc316b7170f63922b9a8e55e9a9a6fc4a7900622
Petar Stoqnov <desireon98@gmail.com>
2023-02-05 14:06:19 +0200

    Save a new record in visitor_history table when user submits multiple choice form.

--- assets/js/components/mode/multiple-choice.vue
+++ assets/js/components/mode/multiple-choice.vue
@@ -13,24 +13,26 @@
     </v-card>
   </div>
   <div class="d-flex flex-column align-center justify-center mx-auto mt-3">
-    <v-radio-group>
+    <v-radio-group v-model="selectedAnswer">
       <v-radio
           color="primary"
-          v-for="questionSuggestion in visitor.nextQuestion.questionSuggestions"
-          :key="questionSuggestion['@id']"
-          :label="questionSuggestion.answer.name"
-          :value="questionSuggestion['@id']">
+          v-for="option in options"
+          :key="option['@id']"
+          :label="option.answer.name"
+          :value="option.answer['@id']">
       </v-radio>
     </v-radio-group>
   </div>
   <div class="d-flex justify-space-around">
-    <v-btn color="primary">
+    <v-btn color="primary" @click="saveHistory">
       Submit
     </v-btn>
   </div>
 </template>
 
 <script>
+
+import axios from "axios";
 
 export default {
   name: 'multipleChoice',
@@ -41,9 +43,27 @@ export default {
     },
   },
   emits: ['change-view'],
+  data() {
+    return {
+      selectedAnswer: null,
+      options: this.visitor.nextQuestion.questionSuggestions
+    }
+  },
   methods: {
     changeView(mode) {
       this.$emit('change-view', mode);
+    },
+    async saveHistory() {
+      try {
+        const response = await axios.post('/api/visitor_histories', {
+          'visitor': this.visitor['@id'],
+          'question': this.visitor.nextQuestion['@id'],
+          'answer': this.selectedAnswer
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
 };
