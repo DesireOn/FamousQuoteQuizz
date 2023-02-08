4bbf6c1de76e09015c88c6ca3c8dbb7230631b66
Petar Stoqnov <desireon98@gmail.com>
2023-02-08 16:59:45 +0200

    Create a new startAgain component.

---
+++ assets/js/components/mode/startAgain.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex justify-space-around">
+    <v-btn color="primary" @click="startAgain" class="mt-3">
+      Start Again
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'startAgain',
+  emits: ['start-again'],
+  methods: {
+    startAgain() {
+      this.$emit('start-again');
+    }
+  }
+}
+</script>
