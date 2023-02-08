2e54131733ac761e4d67006af7b9b01250468b5b
Petar Stoqnov <desireon98@gmail.com>
2023-02-08 17:04:47 +0200

    Create a new stats component.

---
+++ assets/js/components/mode/stats.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="mx-auto mt-3" color="green">
+    <v-card-text>Congrats!</v-card-text>
+    <v-card-text>You have successfully completed our quiz!</v-card-text>
+    <v-card-text>Number of correct answers: {{ scoring.numberOfCorrectAnswers }}</v-card-text>
+    <v-card-text>Number of wrong answers: {{ scoring.numberOfWrongAnswers }}</v-card-text>
+    <v-card-text>Your success rate is: {{ scoring.successRate }}%</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'stats',
+  props: {
+    scoring: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
